08c539716f499914b54107ade144b6f86a0da313
aderayevans <aderayevans@gmail.com>
2023-01-02 02:38:05 +0700

    [SDL2] init

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::cout << "Testing << " << std::endl;
+    std::cin.get();
+    return 0;
+}
